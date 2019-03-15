718dcd9bdffc7815fa6871b7266df37ad75f60a8
chris <chris.castor.us@gmail.com>
2019-03-15 16:59:18 -0400

    begining user login work

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users =  require('./routes/api/users');
+const profile =  require('./routes/api/profiles');
+const posts =  require('./routes/api/posts');
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-//db config
 const db = require('./config/keys').mongoURI;
 mongoose
    .connect(db)
@@ -11,6 +18,10 @@ mongoose
    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
