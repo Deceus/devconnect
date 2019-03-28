00e0a1b4ec271467ba077d906e778916cb3cf320
chris <chris.castor.us@gmail.com>
2019-03-28 19:21:00 -0400

    correct an issue wregarding dev keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 const prodKeys = require('./keys_prod');
-const devKeys = require('./keys_dev');
 
 if (process.env.NODE_ENV === 'production') {
     module.exports = prodKeys;
 } else {
-    module.exports = devKeys;
+    module.exports = require('./keys_dev');
 }
