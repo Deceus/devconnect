cd078d5795e186e6ae77eb9ff05fb914194ab432
chris <chris.castor.us@gmail.com>
2019-03-28 19:46:42 -0400

    removed chrome only redux middleware

--- client/src/store.js
+++ client/src/store.js
@@ -7,7 +7,8 @@ const middleware = [thunk];
 
 export const store = createStore(rootReducer,
     initialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    compose(applyMiddleware(...middleware)));
+
+    // compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
